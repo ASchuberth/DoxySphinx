5781dcf1f5e30e5a0c7ddea7aeff6cdd31843f37
Austin Schuberth <amschuberth@gmail.com>
2026-02-17 23:45:38 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,20 @@ release = '0.1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+import subprocess, os
+
+
+breathe_projects = {}
+
+read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+
+if read_the_docs_build:
+
+     subprocess.call('cd ..;doxygen', shell=True)
+     breathe_projects['doxysphinx'] = '../build/xml'
+
+
+
 extensions = ['breathe']
 
 templates_path = ['_templates']
